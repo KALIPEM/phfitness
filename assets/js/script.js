af273f58377d023eaf8081fc5237d96512a8c4bb
Kalidasan Potti E M <84499084+KALIPEM@users.noreply.github.com>
2023-10-15 22:49:49 +0530

    5th commit

---
+++ assets/js/script.js
@@ -0,0 +1,107 @@
+//text animation -------------
+
+const elts = {
+	text1: document.getElementById("text1"),
+	text2: document.getElementById("text2")
+};
+
+// The strings to morph between. You can change these to anything you want!
+const texts = [
+	"Who we are",
+	"How we Do it",
+	"Why we do it",
+	
+];
+
+// Controls the speed of morphing.
+const morphTime = 1;
+const cooldownTime = 0.75;
+
+let textIndex = texts.length - 1;
+let time = new Date();
+let morph = 0;
+let cooldown = cooldownTime;
+
+elts.text1.textContent = texts[textIndex % texts.length];
+elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+
+function doMorph() {
+	morph -= cooldown;
+	cooldown = 0;
+	
+	let fraction = morph / morphTime;
+	
+	if (fraction > 1) {
+		cooldown = cooldownTime;
+		fraction = 1;
+	}
+	
+	setMorph(fraction);
+}
+
+// A lot of the magic happens here, this is what applies the blur filter to the text.
+function setMorph(fraction) {
+	// fraction = Math.cos(fraction * Math.PI) / -2 + .5;
+	
+	elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+	elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+	
+	fraction = 1 - fraction;
+	elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+	elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+	
+	elts.text1.textContent = texts[textIndex % texts.length];
+	elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+}
+
+function doCooldown() {
+	morph = 0;
+	
+	elts.text2.style.filter = "";
+	elts.text2.style.opacity = "100%";
+	
+	elts.text1.style.filter = "";
+	elts.text1.style.opacity = "0%";
+}
+
+// Animation loop, which is called every frame.
+function animate() {
+	requestAnimationFrame(animate);
+	
+	let newTime = new Date();
+	let shouldIncrementIndex = cooldown > 0;
+	let dt = (newTime - time) / 1000;
+	time = newTime;
+	
+	cooldown -= dt;
+	
+	if (cooldown <= 0) {
+		if (shouldIncrementIndex) {
+			textIndex++;
+		}
+		
+		doMorph();
+	} else {
+		doCooldown();
+	}
+}
+
+// Start the animation.
+animate();
+
+ // Navbar element active state
+
+ let navbarlinks = select('#navbar .scrollto', true)
+ const navbarlinksActive = () => {
+   let position = window.scrollY + 200
+   navbarlinks.forEach(navbarlink => {
+     if (!navbarlink.hash) return
+     let section = select(navbarlink.hash)
+     if (!section) return
+     if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+       navbarlink.classList.add('active')
+     } else {
+       navbarlink.classList.remove('active')
+     }
+   })
+ }
